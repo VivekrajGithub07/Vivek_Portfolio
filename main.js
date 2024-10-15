3759367028626ea01cc45f5a6f015c14e523c455
VivekrajGithub07 <155048011+VivekrajGithub07@users.noreply.github.com>
2024-10-15 11:49:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".text",{ 
+    strings: ["Web Developer" , "Python Developer" , "Graphic Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+var mixer = mixitup('.portfolio-gallery');
